671d97cf9524076245a43844caaa9c0a47270ae6
Scesar1 <scesar1@jhu.edu>
2022-09-17 19:07:16 -0400

    Add Map Fragment

--- app/src/main/java/com/example/ham/MainActivity.java
+++ app/src/main/java/com/example/ham/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.ham;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<FusedLocationProviderClient> extends AppCompatActivity {
+
+    private Fragment mapFrag;
+    private FragmentTransaction transaction;
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapFrag = new MapFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction().replace(R.id.fragment_container, mapFrag)
+                .commit();
+
     }
 }
